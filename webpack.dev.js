2797c462d68f0d7aa2bb4539870165a9359149ff
Evgeny Leukhin <e.leukhin@gmail.com>
2021-05-03 11:57:52 +0600

    Add webpack-dev-server and html-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const paths = {
   SRC: path.resolve(__dirname, 'src/App.js'),
   DIST: path.resolve(__dirname, 'dist')
@@ -7,6 +8,18 @@ const paths = {
 module.exports = {
   mode: 'development',
   entry: paths.SRC,
+  devServer: {
+    open: true,
+    contentBase: paths.DIST,
+    compress: true,
+    // host: '0.0.0.0',
+    port: 8888,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    hot: true,
+  },
   output: {
     path: paths.DIST,
     publicPath: '', // src="publicPath/bundle.js"
@@ -21,4 +34,11 @@ module.exports = {
       },
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body', // head
+    })
+  ]
 }
